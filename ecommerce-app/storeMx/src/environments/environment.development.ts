3369bd6c9d6152dadfa4b41c67aa61d028d20ae5
Bryan Sebastián Ramos González <sebastianramos6991@gmail.com>
2025-12-16 19:11:23 -0600

    Fix: Configurar environments correctamente para Prod y Dev

--- ecommerce-app/storeMx/src/environments/environment.development.ts
+++ ecommerce-app/storeMx/src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: false, // O true, dependiendo de cuál sea
-  apiUrl: 'http://localhost:3000/api' // 👈 CAMBIA 'BACK_URL' POR 'apiUrl'
+  production: false,
+  apiUrl: 'http://localhost:3000/api' 
 };
